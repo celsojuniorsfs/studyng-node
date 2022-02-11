902f783c9088be842f8cc017c5600af6414c9005
Celso Junior <celso.junior@flashcover.com.br>
2022-02-11 13:47:50 -0300

    correcao app.ts

--- app.ts
+++ app.ts
@@ -1,3 +1,11 @@
-let message: string = "Olá, Mundo!";
+const express = require("express");
+const app = express();
+const port = 3000;
 
-console.log(message);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
